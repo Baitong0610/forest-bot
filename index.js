4a8d85d3c58a83ba9663ce46eebc835f851ef613
Baitong0610 <journeywfriends.81@gmail.com>
2025-04-25 15:24:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ async function handleEvent(event) {
 }
 
 // ✅ Start server
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`🌳 Forest bot running on port ${port}`);
 });
